f25513bd8d528eb392969e07f8aa789ea041a9cc
reka <debreka@gmail.com>
2022-06-08 14:17:28 +0200

    refactor order class

--- src/main/java/com/codecool/shop/model/shoppingcart/Order.java
+++ src/main/java/com/codecool/shop/model/shoppingcart/Order.java
@@ -1,5 +1,6 @@
 package com.codecool.shop.model.shoppingcart;
 
+import com.codecool.shop.model.BaseModel;
 import com.codecool.shop.model.Product;
 
 import java.math.BigDecimal;
@@ -8,16 +9,15 @@ import java.util.UUID;
 
 import static java.lang.Integer.valueOf;
 
-public class Order {
-    private Map<Product, Integer> orderedItems;
+public class Order{
+    private Cart carttoOrder;
     private BigDecimal purchasePrice;
-    private UUID id;
+  //  private int id;
 
 
-    public Order(Map<Product, Integer> orderedItems, BigDecimal purchasePrice) {
-        this.orderedItems = orderedItems;
+    public Order(Cart cart, BigDecimal purchasePrice) {
+        this.carttoOrder = cart;
         this.purchasePrice = purchasePrice;
-        this.id = UUID.randomUUID();
 
     }
 
@@ -25,9 +25,8 @@ public class Order {
     @Override
     public String toString() {
         var strBuilder = new StringBuilder("Order details: { order id: ")
-                .append(id)
                 .append("\n");
-        orderedItems.forEach((item, amount) -> {
+        carttoOrder.getLineItems().forEach((item, amount) -> {
             strBuilder.append(item)
                     .append(": ")
                     .append(amount)
@@ -36,6 +35,6 @@ public class Order {
                     .append(purchasePrice)
                     .append("}");
         });
-        return (orderedItems.size() > 0 ? strBuilder.toString() : "There is nothing in ordered items \n");
+        return (carttoOrder.getLineItems().size() > 0 ? strBuilder.toString() : "There is nothing in ordered items \n");
     }
 }
