ff2a6ca2462030c8f81a5b03272e86e89347e559
kmnczr <minczerkristof@gmail.com>
2022-06-09 14:14:07 +0200

    Refactor

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -2,27 +2,17 @@
 console.warn("asd")
 function initEventListeners() {
     const cartButtons = document.getElementsByClassName("add-to-cart");
-    console.log(cartButtons);
     //iterate over cart buttons
     for (let i = 0; i < cartButtons.length; i++) {
-        debugger;
         cartButtons[i].addEventListener("click",async function(){
             await apiGET("/cart/add?productId="+cartButtons[i].getAttribute("id"))
     })}
-
-    // cartButtons.forEach(cartButton => {
-    //     cartButton.addEventListener("click",async function(){
-    //         await apiGET("/cart/add?productId="+cartButton.getAttribute("id"));
-    //
-    //     } );
-    // })
 
 }
 
 
 async function apiGET(url) {
     const response = await fetch(url, {method: "GET"});
-    // return await response.json();
 }
 
 function main() {
