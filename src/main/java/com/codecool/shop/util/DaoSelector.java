c8fba96f48aaf3eaf1f827fbfa125c1a49b3d645
Kelemen Angi <angela.kelemen.nemeth@gmail.com>
2022-06-21 18:38:21 +0200

    Add databasemanager functionality with connect method

---
+++ src/main/java/com/codecool/shop/util/DaoSelector.java
@@ -0,0 +1,61 @@
+package com.codecool.shop.util;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.Jdbc.ProductCategoryJdbc;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
+import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.service.CartService;
+import com.codecool.shop.service.ProductService;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DaoSelector {
+    private static ProductCategoryDao ProductCategoryDao;
+    private static ProductDao ProductDao;
+    private static SupplierDao SupplierDao;
+    private static CartDao CartDao;
+    private static Properties properties = new Properties();
+
+
+    public static ProductService getService() throws IOException {
+        properties.load(new FileInputStream("src/main/resources/connection.properties"));
+        if(properties.getProperty("dao").equals("jdbc")){
+            try {
+                DataSource dataSource = connect();
+                ProductCategoryDao = new ProductCategoryJdbc(dataSource);
+
+            } catch (SQLException e) {
+                System.err.println("Database connection unavailable!");
+            }
+        }
+        else if (properties.getProperty("dao").equals("memory")) {
+            ProductDao = ProductDaoMem.getInstance();
+            ProductCategoryDao = ProductCategoryDaoMem.getInstance();
+            SupplierDao = SupplierDaoMem.getInstance();
+        }
+        return new ProductService(ProductDao, ProductCategoryDao, SupplierDao);
+    }
+
+
+    public static DataSource connect() throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setDatabaseName(properties.getProperty("db_name"));
+        dataSource.setUser(properties.getProperty("db_user"));
+        dataSource.setPassword(properties.getProperty("db_password"));
+        System.out.println("Trying to connect...");
+        dataSource.getConnection().close();
+        System.out.println("Connection OK");
+
+        return dataSource;
+    }
+}
